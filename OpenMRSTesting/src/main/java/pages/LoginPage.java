015f55994e70d5534c914b7d3d023aa1038a99ec
KTech3020 <98096970+KTech3020@users.noreply.github.com>
2023-07-21 14:22:28 +0700

    Finished BookAppointment(error) and CaptureVitals

--- OpenMRSTesting/src/main/java/pages/LoginPage.java
+++ OpenMRSTesting/src/main/java/pages/LoginPage.java
@@ -2,12 +2,9 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 public class LoginPage {
-    private WebDriver driver;
+    private final WebDriver driver;
     private final By usernameField = By.id("username");
     private final By passwordField = By.id("password");
     private final By inpatientWardSelect = By.id("Inpatient Ward");
@@ -32,6 +29,11 @@ public class LoginPage {
         driver.findElement(loginButton).click();
         return new DashboardPage(driver);
     }
-
+    public DashboardPage doSuccessfulLogin(){
+        setUserName("Admin");
+        setPassword("Admin123");
+        chooseInpatientWard();
+        return clickLoginButton();
+    }
 
 }
--- OpenMRSTesting/src/test/java/base/BaseTest.java
+++ OpenMRSTesting/src/test/java/base/BaseTest.java
@@ -1,19 +1,39 @@
 package base;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.AppointmentSchedulingPage;
+import pages.DashboardPage;
 import pages.LoginPage;
 
 import java.time.Duration;
 
 public class BaseTest {
-    WebDriver driver;
-    protected LoginPage loginPage;
+    private WebDriver driver;
+
+    protected  LoginPage loginPage;
+    protected DashboardPage dashboardPage;
+
+    public void prepareTests(){
+        driver = new ChromeDriver();
+        driver.get("https://demo.openmrs.org/openmrs/login.htm");
+        driver.manage().window().maximize();
+        loginPage = new LoginPage(driver);
+        dashboardPage = loginPage.doSuccessfulLogin();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("appointmentschedulingui-homeAppLink-appointmentschedulingui-homeAppLink-extension")));
+        dashboardPage.addDummyPatientRequest();
+        AppointmentSchedulingPage appointmentSchedulingPage = dashboardPage.clickAppointmentScheduling();
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("appointmentschedulingui-scheduleProviders-app")));
+        appointmentSchedulingPage.createDummyAppointmentSchedule();
+        driver.quit();
+    }
 
     @BeforeEach
     public void setup() {
@@ -21,8 +41,10 @@ public class BaseTest {
         driver.get("https://demo.openmrs.org/openmrs/login.htm");
         driver.manage().window().maximize();
         loginPage = new LoginPage(driver);
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-        wait.until(ExpectedConditions.elementToBeClickable(By.id("loginButton")));
+        dashboardPage = loginPage.doSuccessfulLogin();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("appointmentschedulingui-homeAppLink-appointmentschedulingui-homeAppLink-extension")));
+
     }
 
     @AfterEach
@@ -30,10 +52,8 @@ public class BaseTest {
         driver.quit();
     }
 
-    public void doSuccessfulLogin(){
-        loginPage.setUserName("Admin");
-        loginPage.setPassword("Admin123");
-        loginPage.chooseInpatientWard();
-        loginPage.clickLoginButton();
+    public static void main(String[] args){
+        BaseTest test = new BaseTest();
+        test.prepareTests();
     }
 }
